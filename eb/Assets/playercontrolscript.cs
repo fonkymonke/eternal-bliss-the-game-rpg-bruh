62f736e5e37bda00c39e14f2aee67d4046dc6f54
fonkymonke <128927441+fonkymonke@users.noreply.github.com>
2023-03-27 00:55:14 +0800

    character movement
    movement hee hee ha ha

--- eb/Assets/playercontrolscript.cs
+++ eb/Assets/playercontrolscript.cs
@@ -10,34 +10,66 @@ public class playercontrolscript : MonoBehaviour
     public ContactFilter2D movementFilter;
 
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    Animator animator;
+
+
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate() {
-    {
+    
         if (movementInput != Vector2.zero){
-       
-            int count = rb.Cast(
-                movementInput,
+              bool success =  TryMove(movementInput);
+
+              if(!success) {
+                success = TryMove(new Vector2(movementInput.x, 0));
+             if(!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+              }
+                
+        animator.SetBool("moving", success);
+        } else {
+            animator.SetBool("moving", false);
+
+    }
+// direction of sprite to movement direction
+if(movementInput.x < 0){
+    spriteRenderer.flipX = true;
+} else if (movementInput.x > 0) {
+    spriteRenderer.flipX = false;
+    }
+}
+private bool TryMove(Vector2 direction) {
+    if(direction != Vector2.zero) {
+     int count = rb.Cast(
+                direction,
                 movementFilter,
                 castCollisions,
                 moveSpeed * Time.fixedDeltaTime + collisionOffset);
 
             if(count == 0){
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);       
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);  
+                return true;
+            } else {
+                return false;
+            }     
+        } else {
+            // Can't move if there's no direction to move in
+            return false;
         }
-    }
-    }
-    }
+}
 
-    void OnMove(InputValue movement)
-    {
+    void OnMove(InputValue movement){
         movementInput = movement.Get<Vector2>();
     }
 }
